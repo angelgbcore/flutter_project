62bb06807042e354ce41e8af12c5175630d649a5
FlutLab.io <>
2021-09-10 17:32:54 +0000

    Autosave

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Título'),
