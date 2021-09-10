4222deec30c0d49aba8e6847755bbfb04168fb8b
FlutLab.io <>
2021-09-10 18:01:39 +0000

    Autosave

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -8,21 +8,28 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Título'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-            Text(
-              'Número de taps:',
-              style: estiloTexto,
-            ),
-            Text(
-              '0',
-              style: estiloTexto,
-            ),
-          ]),
-        ));
+      appBar: AppBar(
+        title: Text('Título'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+          Text(
+            'Número de taps:',
+            style: estiloTexto,
+          ),
+          Text(
+            '0',
+            style: estiloTexto,
+          ),
+        ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print('Hola mundo!');
+        },
+      ),
+    );
   }
 }
