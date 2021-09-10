1b8eacb3b889a74aa0c1bc1c985ee13d031b8b2f
FlutLab.io <>
2021-09-10 00:08:08 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(context) {
     return MaterialApp(
-        home: Center(
-      child: Text("Hola mundo"),
-    ));
+      home: Center(
+        child: Text("Hola mundo"),
+      ),
+    );
   }
 }
