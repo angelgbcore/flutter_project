467e1bbd61342b9a7b0b9eb2084be346b7409259
FlutLab.io <>
2021-09-10 00:32:08 +0000

    Autosave

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:contador/src/pages/home_page.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(context) {
     return MaterialApp(
       home: Center(
-        child: Text("¡Hola Gabo!"),
+        child: HomePage(),
       ),
     );
   }
