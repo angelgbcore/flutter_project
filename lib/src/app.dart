512bef58cf6d5d535ef0e9ab780d225ba36a8a6d
FlutLab.io <>
2021-09-13 18:23:30 +0000

    Autosave

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 // nuestras importaciones
-import 'pages/home_page.dart';
+//import 'pages/home_page.dart';
 import 'pages/contador_page.dart';
 
 class MyApp extends StatelessWidget {
