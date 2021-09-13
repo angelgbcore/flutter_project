69df8927715e54bcea082407ae2dc79410499804
FlutLab.io <>
2021-09-13 18:32:03 +0000

    Autosave

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -7,12 +7,6 @@ import 'pages/contador_page.dart';
 class MyApp extends StatelessWidget {
   @override
   Widget build(context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Center(
-        //child: HomePage(),
-        child: ContadorPage(),
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: ContadorPage());
   }
 }
